0971a2c5651871f3e2c52795e720a3fa05d995c4
James Beswick <james@192.168.1.173>
2021-09-22 21:02:01 +0100

    nextjs changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "77d6dd5d98acb95284e6";
+/******/ 	var hotCurrentHash = "7130c88f7a03b2f205c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
